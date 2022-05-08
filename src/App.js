c45c2cd416d78f76ce68c79cf881388c236891a5
roivasxxx <tiragoshi@gmail.com>
2022-05-08 17:22:44 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./index.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="flex">
+        <h1 className="text-purple-700">Hello react</h1>
+        <h1>Test</h1>
+        <h1>Test2</h1>
+      </div>
     </div>
   );
 }
