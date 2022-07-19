5c797502676bf380f33d27b6499426ddc4bbf0f8
roivasxxx <tiragoshi@gmail.com>
2022-07-19 12:42:51 +0200

    prettier config rename, code formatting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import "./App.css";
-import "./index.css";
-import { useState, useEffect } from "react";
-import { HashRouter, Outlet } from "react-router-dom";
-import Router from "./routing/Router";
-import MainWindow from "./components/windows/MainWindow";
-import ContextProvider from "./context/DataProvider";
+import './App.css'
+import './index.css'
+import { useState, useEffect } from 'react'
+import { HashRouter, Outlet } from 'react-router-dom'
+import Router from './routing/Router'
+import MainWindow from './components/windows/MainWindow'
+import ContextProvider from './context/DataProvider'
 
 function App() {
-  
-  
-  useEffect(() => {
-    //window.electron.sendBack("returnData", (data) => setText(data));
-  }, []);
-  
-      
-      return (
+    useEffect(() => {
+        // window.electron.sendBack("returnData", (data) => setText(data));
+    }, [])
+
+    return (
         <HashRouter>
-      <ContextProvider>     
-      <MainWindow/>
+            <ContextProvider>
+                <MainWindow />
             </ContextProvider>
-    </HashRouter>
-  );
+        </HashRouter>
+    )
 }
 
-export default App;
+export default App
