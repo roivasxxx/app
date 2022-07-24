4a6e91868f0ab2b61d758ebaed9b8d72024b910e
roivasxxx <tiragoshi@gmail.com>
2022-07-24 22:16:57 +0200

    frameless window, installed ionicons icons

--- public/electron.js
+++ public/electron.js
@@ -11,6 +11,7 @@ function createWindow() {
     mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
+        frame: false,
         webPreferences: {
             nodeIntegration: false,
             contextIsolation: true,
