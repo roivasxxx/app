29549c33ce12fe6582ab469c814cb822189d6dfa
roivasxxx <tiragoshi@gmail.com>
2022-07-28 17:29:58 +0200

    frameless window - custom frame button icons

---
+++ src/components/general/CustomIcon.js
@@ -0,0 +1,23 @@
+export default function CustomIcon({ className, icon, style, size }) {
+    return (
+        <div className={className}>
+            <svg
+                className="no_drag fill-current h-6 align-middle m-2"
+                style={style}
+                viewBox="0 0 24 24"
+                width={`${size}px`}
+                height={`${size}px`}
+                xmlns="http://www.w3.org/2000/svg"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+            >
+                <path d={icons[icon]} />
+            </svg>
+        </div>
+    )
+}
+
+const icons = {
+    close: 'M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z',
+    minify: 'M6 19h12v2H6z',
+    maximize: 'M3,3v18h18V3H3z M19,19H5V5h14V19z',
+}
--- src/routing/NavBar.js
+++ src/routing/NavBar.js
@@ -1,23 +1,37 @@
-import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
+import CustomIcon from '../components/general/CustomIcon'
 
-const textStyle = "text-purple-300 hover:text-purple-500";
+const textStyle = 'text-purple-300 hover:text-purple-500'
+
+const FrameActionBar = () => {
+    const iconSize = 16
+
+    return (
+        <div className={'flex row justify-around items-center my-2'}>
+            <CustomIcon icon="minify" size={iconSize} className={textStyle} />
+            <CustomIcon icon="maximize" size={iconSize} className={textStyle} />
+            <CustomIcon icon="close" size={iconSize} className={textStyle} />
+        </div>
+    )
+}
 
 export default function NavBar() {
-  return (
-    <>
-      <div className="flex row justify-around items-center my-2 bg-gray-600">
-        <Link to="/">
-          <h2 className={textStyle}>Home</h2>
-        </Link>
-        <Link to="/todo">
-          <h2 className={textStyle}>To-do</h2>
-        </Link>
-        <Link to="/notes">
-          <h2 className={textStyle}>Notes</h2>
-        </Link>
-      </div>
-      <Outlet />
-    </>
-  );
+    return (
+        <>
+            <div className="flex row justify-around items-center my-2 bg-gray-600">
+                <Link to="/">
+                    <h2 className={textStyle}>Home</h2>
+                </Link>
+                <Link to="/todo">
+                    <h2 className={textStyle}>To-do</h2>
+                </Link>
+                <Link to="/notes">
+                    <h2 className={textStyle}>Notes</h2>
+                </Link>
+                <FrameActionBar />
+            </div>
+            <Outlet />
+        </>
+    )
 }
